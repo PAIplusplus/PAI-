5026c45fe1dcfa3481f9cfd9ee83492fb77502d9
Paulaherrcos <herreracospaula@gmail.com>
2025-02-04 16:04:36 +0100

    numerotop10

--- js/top10pelis.js
+++ js/top10pelis.js
@@ -1,42 +1,49 @@
-async function obtenerTop10(token){
+async function obtenerTop10(token) {
   try {
-    const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',{
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${token}`,   
-      'Accept': 'application/json'
+    const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json'
       }
     });
-    const data = await response.json();  
+    const data = await response.json();
     return data;
   } catch (error) {
     console.error("Error al obtener las películas top10:", error);
-    return null;  
+    return null;
   }
 }
 
-
 async function mostrarTop10() {
-  const apiToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDNlODQyNDFjNTllODQ5ZjY4MGY2MDRmNTY5Nzc4YiIsIm5iZiI6MTczNzU2MjMyNi45NjgsInN1YiI6IjY3OTExOGQ2ZmI2MDlkOTI2ODI4YzBkMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sGlTMmT37X3Vbw8jcEAJesSa38W4JjEt6yXicjFJDbA";  
-  const data = await obtenerTop10(apiToken);  
+  const apiToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDNlODQyNDFjNTllODQ5ZjY4MGY2MDRmNTY5Nzc4YiIsIm5iZiI6MTczNzU2MjMyNi45NjgsInN1YiI6IjY3OTExOGQ2ZmI2MDlkOTI2ODI4YzBkMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sGlTMmT37X3Vbw8jcEAJesSa38W4JjEt6yXicjFJDbA";
+  const data = await obtenerTop10(apiToken);
 
   if (data && data.results) {
-      const container = document.getElementById("movie-top10-container");  
-      container.innerHTML = ''; 
+    const container = document.getElementById("movie-top10-container");
+    container.innerHTML = '';
 
-      data.results.forEach(pelicula => {
-          const movieElement = document.createElement("div");
-          movieElement.classList.add("movie-item");
+    const top10Peliculas = data.results.slice(0, 10); // Obtener solo las primeras 10
 
-          const posterUrl = `https://image.tmdb.org/t/p/w500${pelicula.poster_path}`;
+    top10Peliculas.forEach((pelicula, index) => {
+      const movieElement = document.createElement("div");
+      movieElement.classList.add("movie-item");
 
-          movieElement.innerHTML = `
-              <img src="${posterUrl}" alt="${pelicula.title}" style="width: 200px; height: 300px;" />
-          `;
-          container.appendChild(movieElement);  
-      });
+      const posterUrl = `https://image.tmdb.org/t/p/w500${pelicula.poster_path}`;
+      const numberImage = `/imagenes/numeros/${index + 1}.webp`;  // La imagen del número
+
+      movieElement.innerHTML = `
+        <div class="movie-poster">
+          <img src="${posterUrl}" alt="${pelicula.title}" style="width: 300px; height: 250px;" />
+        </div>
+        <div class="movie-number">
+          <img src="${numberImage}" />
+        </div>
+      `;
+      container.appendChild(movieElement);
+    });
   } else {
-      console.log("No se pudieron cargar las películas top.");
+    console.log("No se pudieron cargar las películas top.");
   }
 }
 
