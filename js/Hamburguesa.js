c49070a6e77f3c671a1ffb4e0acbf59d067362b1
Irashe0 <iratxe.pastana@juanxxiii.net>
2025-02-18 16:34:29 +0100

    g

--- js/Hamburguesa.js
+++ js/Hamburguesa.js
@@ -1,20 +1,6 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav');
-const dropdown = document.getElementById('dropdown');
-const closeDropdownButton = document.getElementById('close-dropdown');
 
-hamburger.addEventListener('click', function () {
-  this.classList.toggle('active');
+hamburger.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
-
-const dropdownToggle = document.querySelector('.dropdown-toggle');
-dropdownToggle.addEventListener('click', function (e) {
-  e.preventDefault();
-  dropdown.classList.toggle('active');
-});
-
-closeDropdownButton.addEventListener('click', function () {
-  dropdown.classList.remove('active');
-});
-
