138eeda64f681429049fc0c19a27b5c946701b66
Irashe0 <iratxe.pastana@juanxxiii.net>
2025-02-18 16:29:52 +0100

    fsdfs

--- js/Hamburguesa.js
+++ js/Hamburguesa.js
@@ -1,6 +1,20 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav');
+const dropdown = document.getElementById('dropdown');
+const closeDropdownButton = document.getElementById('close-dropdown');
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', function () {
+  this.classList.toggle('active');
   nav.classList.toggle('active');
 });
+
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+dropdownToggle.addEventListener('click', function (e) {
+  e.preventDefault();
+  dropdown.classList.toggle('active');
+});
+
+closeDropdownButton.addEventListener('click', function () {
+  dropdown.classList.remove('active');
+});
+
