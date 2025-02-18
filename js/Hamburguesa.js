8ecd185019c42e4e9cf78ac961379dc1204eecf9
Irashe0 <iratxe.pastana@juanxxiii.net>
2025-02-18 17:20:06 +0100

    LA VIDA ME QUIERE

--- js/Hamburguesa.js
+++ js/Hamburguesa.js
@@ -1,6 +1,32 @@
-const hamburger = document.getElementById('hamburger');
+const hamburgerBtn = document.getElementById('hamburger-btn');
 const nav = document.getElementById('nav');
+const dropdown = document.getElementById('dropdown');
 
-hamburger.addEventListener('click', () => {
+
+hamburgerBtn.addEventListener('click', function () {
   nav.classList.toggle('active');
+
+
+  if (nav.classList.contains('active')) {
+    hamburgerBtn.innerHTML = '<i class="bi bi-x"></i>';
+  } else {
+    hamburgerBtn.innerHTML = '<i class="bi bi-list"></i>';
+  }
 });
+
+
+const dropdownToggle = document.querySelector('.dropdown-toggle');
+dropdownToggle.addEventListener('click', function (e) {
+  e.preventDefault();
+  dropdown.classList.toggle('active');
+});
+
+
+window.addEventListener('click', function (e) {
+  if (!nav.contains(e.target) && !hamburgerBtn.contains(e.target) && !dropdown.contains(e.target)) {
+    nav.classList.remove('active');
+    hamburgerBtn.innerHTML = '<i class="bi bi-list"></i>';
+    dropdown.classList.remove('active');
+  }
+});
+
