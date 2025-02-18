a864dd225cb3f203e8ee5049acbc846bd2c07ce9
Irashe0 <iratxe.pastana@juanxxiii.net>
2025-02-18 17:26:48 +0100

    ahora si que si funciona

--- js/Hamburguesa.js
+++ js/Hamburguesa.js
@@ -1,32 +1,6 @@
-const hamburgerBtn = document.getElementById('hamburger-btn');
+const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav');
-const dropdown = document.getElementById('dropdown');
 
-
-hamburgerBtn.addEventListener('click', function () {
+hamburger.addEventListener('click', () => {
   nav.classList.toggle('active');
-
-
-  if (nav.classList.contains('active')) {
-    hamburgerBtn.innerHTML = '<i class="bi bi-x"></i>';
-  } else {
-    hamburgerBtn.innerHTML = '<i class="bi bi-list"></i>';
-  }
 });
-
-
-const dropdownToggle = document.querySelector('.dropdown-toggle');
-dropdownToggle.addEventListener('click', function (e) {
-  e.preventDefault();
-  dropdown.classList.toggle('active');
-});
-
-
-window.addEventListener('click', function (e) {
-  if (!nav.contains(e.target) && !hamburgerBtn.contains(e.target) && !dropdown.contains(e.target)) {
-    nav.classList.remove('active');
-    hamburgerBtn.innerHTML = '<i class="bi bi-list"></i>';
-    dropdown.classList.remove('active');
-  }
-});
-
